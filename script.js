a974dd7632f493a3b98605c28d7dfcf41da867f6
Brittany Hanlon <bmph1997@hotmail.com>
2023-08-24 13:37:20 -0230

    score function edit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// timer function
 var timer;
 var ele = document.getElementById("timer");
 
@@ -13,4 +14,19 @@ function pause() {
   clearInterval(timer);
 }
 
+// score function
 var score;
+var ele = document.getElementById ("score")
+
+(function (){
+    var score = 0;
+    score = setInterval (() => {
+        ele.innerHTML = "00" + score
+        score++;
+    }, 1) // 1 point each
+})();
+
+function Reset (){
+    clearInterval(score)
+}
+    
