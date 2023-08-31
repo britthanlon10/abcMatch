b71b77d22f28a9d6649557369e8f0a6386908a8b
Brittany Hanlon <bmph1997@hotmail.com>
2023-08-31 13:47:19 -0230

    js changes

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const OPERATION = {
-    Next : "next",
-    Back : "back",
-    Skip : "skip",
-    Pause : "pause",
-    Help : "help"
+  Next: "next",
+  Back: "back",
+  Skip: "skip",
+  Pause: "pause",
+  Help: "help",
 };
-function buttonbox () {
-    document.getElementById ("next")
-    document.getElementById ("back")
-    document.getElementById ("skip")
-    document.getElementById ("pause")
-    document.getElementById ("help")
-    
+function buttonbox() {
+  document.getElementById("next");
+  document.getElementById("back");
+  document.getElementById("skip");
+  document.getElementById("pause");
+  document.getElementById("help");
+}
+
+function js_timer() {
+  var sec = 0;
+  timer = setInterval(
+    () => {
+      ele.innerHTML = "00" + sec;
+      sec++;
+    },
+    1000 //each 1 second
+  );
 }
