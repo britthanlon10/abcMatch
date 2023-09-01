52e2212f5b0a4e79dbd2192b5d39417bae6168ca
Brittany Hanlon <bmph1997@hotmail.com>
2023-09-01 17:46:48 -0230

    html/css/js changes

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ function buttonbox() {
   document.getElementById("help");
 }
 
-function js_timer() {
+var timer;
+var ele = document.getElementById('timer');
+
+(function (){
   var sec = 0;
-  timer = setInterval(
-    () => {
-      ele.innerHTML = "00" + sec;
-      sec++;
-    },
-    1000 //each 1 second
-  );
+  timer = setInterval(()=>{
+    ele.innerHTML = '00:'+sec;
+    sec ++;
+  }, 1000) // each 1 second
+})() 
+
+function pause(){
+  clearInterval(timer);
 }
