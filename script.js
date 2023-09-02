f80d6a7d2819da394cbeb52ae37981dae7e13109
Brittany Hanlon <bmph1997@hotmail.com>
2023-09-01 21:30:07 -0230

    js changes

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-const OPERATION = {
+const operation = {
   Next: "next",
   Back: "back",
   Skip: "skip",
   Pause: "pause",
   Help: "help",
 };
+
+// counting time spent playing game
+let timer;
+let ele = document.getElementById("timer");
+
+(function () {
+  var sec = 0;
+  timer = setInterval(() => {
+    ele.innerHTML = "00:" + sec;
+    sec++;
+  }, 1000); // each 1 second
+})();
+
+function pause() {
+  clearInterval(timer);
+}
+
 function buttonbox() {
   document.getElementById("next");
   document.getElementById("back");
   document.getElementById("skip");
   document.getElementById("pause");
   document.getElementById("help");
-}
-
-var timer;
-var ele = document.getElementById('timer');
-
-(function (){
-  var sec = 0;
-  timer = setInterval(()=>{
-    ele.innerHTML = '00:'+sec;
-    sec ++;
-  }, 1000) // each 1 second
-})() 
-
-function pause(){
-  clearInterval(timer);
 }
