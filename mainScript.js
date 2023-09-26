200e6ae3fa8fe2c5d199589c9aa3e8114924bcb7
Brittany Hanlon <bmph1997@hotmail.com>
2023-09-26 13:44:13 -0230

    score function edit

--- mainScript.js
+++ mainScript.js
@@ -1,10 +1,19 @@
 // Timer code source: https://daily-dev-tips.com/posts/vanilla-javascript-timer/#google_vignette
+let score = 0;
+let time;
 
 const imageList = [
   "alligator","balloons","cat","dolphin","elephant",
   "flower","goat","horse","icecream","jellyfish","koala","ladybug",
   "monkey","numbers","octopus","penguin","queen","raccoon","snail",
   "turtle","unicorn","violin","whale","xylophone","zebra"
+];
+
+const imageWorth = [
+  10, 5, 5, 10, 5,
+  5, 10, 10, 5, 10, 10, 10,
+  10, 15, 15, 10, 15, 10, 5,
+  5, 10, 15, 10, 10, 10
 ];
 
 const alphList = [
@@ -25,30 +34,25 @@ startTimer = () => {
 
   timerInterval = setInterval(function () {
   
-    // two digit numbers
     timer.innerHTML =
       (hour ? hour + ":" : "") +
       (minute < 10 ? "0" + minute : minute) +
       ":" +
       (second < 10 ? "0" + second : second);
 
-    // + seconds
     second++;
 
-    // one minute roll over
     if (second == 60) {
-      // If so, we add a minute and reset our seconds to 0
       minute++;
       second = 0;
     }
 
-    // If we hit 60 minutes "one hour" we reset the minutes and plus an hour
-    if (minute == 60) {
-      hour++;
-      minute = 0;
-    }
   }, 1000);
 };
+
+// score function 
+
+
 
 function initHelp () {
   alert ("Game Play: Try to find a picture card that matches the letter shown. For example: If shown the letter A, select the picture that shows something that starts with the letter A, like an apple. A correct answer will add a point to the score; if incorrect a point will be subtracted from the score.")
