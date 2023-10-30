6eca3ab0dc0b6ad3593f1c989b0df7481b3f9094
Brittany Hanlon <bmph1997@hotmail.com>
2023-10-29 22:24:29 -0230

    js imagesArray and css changes

--- mainScript.js
+++ mainScript.js
@@ -28,150 +28,26 @@ const imagesArray = [
   "Images/cat.svg",
   "Images/dolphin.svg",
   "Images/elephant.svg",
-  // {
-  //     id: 1,
-  //     image: 'Images/balloons.svg',
-  //     name: 'balloons',
-
-  // },
-  // {
-  //     id: 2,
-  //     image: 'images/cat.svg',
-  //     name: 'cat',
-
-  // },
-  // {
-  //     id: 3,
-  //     image: 'images/dolphin.svg',
-  //     name: 'dolphin',
-
-  // },
-  // {
-  //     id: 4,
-  //     image: 'images/elephant.svg',
-  //     name: 'elephant',
-
-  // },
-  // {
-  //     id: 5,
-  //     image: 'images/flower.svg',
-  //     name: 'flower',
-
-  // },
-  // {
-  //     id: 6,
-  //     image: 'images/goat.svg',
-  //     name: 'goat',
-
-  // },
-  // {
-  //     id: 7,
-  //     image: 'images/horse.svg',
-  //     name: 'horse',
-
-  // },
-  // {
-  //     id: 8,
-  //     image: 'images/icecream.svg',
-  //     name: 'icecream',
-
-  // },
-  // {
-  //     id: 9,
-  //     image: 'images/jellyfish.svg',
-  //     name: 'jellyfish',
-
-  // },
-  // {
-  //     id: 10,
-  //     image: 'images/koala.svg',
-  //     name: 'koala',
-
-  // },
-  // {
-  //     id: 11,
-  //     image: 'images/ladybug.svg',
-  //     name: 'ladybug',
-
-  // },
-  // {
-  //     id: 12,
-  //     image: 'images/monkey.svg',
-  //     name: 'monkey',
-
-  // },
-  // {
-  //     id: 13,
-  //     image: 'images/numbers.svg',
-  //     name: 'numbers',
-
-  // },
-  // {
-  //     id: 14,
-  //     image: 'images/octopus.svg',
-  //     name: 'octopus',
-
-  // },
-  // {
-  //     id: 15,
-  //     image: 'images/penguin.svg',
-  //     name: 'penguin',
-
-  // },
-  // {
-  //     id: 16,
-  //     image: 'images/queen.svg',
-  //     name: 'queen',
-
-  // },
-  // {
-  //     id: 17,
-  //     image: 'images/raccoon.svg',
-  //     name: 'raccoon',
-
-  // },
-  // {
-  //     id: 18,
-  //     image: 'images/snail.svg',
-  //     name: 'snail',
-
-  // },
-  // {
-  //     id: 19,
-  //     image: 'images/turtle.svg',
-  //     name: 'turtle',
-
-  // },
-  // {
-  //     id: 20,
-  //     image: 'images/unicorn.svg',
-  //     name: 'unicorn',
-
-  // },
-  // {
-  //     id: 21,
-  //     image: 'images/violin.svg',
-  //     name: 'violin',
-
-  // },
-  // {
-  //     id: 22,
-  //     image: 'images/whale.svg',
-  //     name: 'whale',
-
-  // },
-  // {
-  //     id: 23,
-  //     image: 'images/xylophone.svg',
-  //     name: 'xylophone',
-
-  // },
-  // {
-  //     id: 24,
-  //     image: 'images/zebra.svg',
-  //     name: 'zebra',
-
-  // }
+  "Images/flower.svg",
+  "Images/horse.svg",
+  "Images/icecream.svg",
+  "Images/jellyfish.svg",
+  "Images/koala.svg",
+  "Images/ladybug.svg",
+  "Images/monkey.svg",
+  "Images/numbers.svg",
+  "Images/octopus.svg",
+  "Images/penguin.svg",
+  "Images/queen.svg",
+  "Images/raccoon.svg",
+  "Images/snail.svg",
+  "Images/turtle.svg",
+  "Images/unicorn.svg",
+  "Images/violin.svg",
+  "Images/whale.svg",
+  "Images/xylophone.svg",
+  "Images/yak.svg",
+  "Images/zebra.svg"
 ];
 
 const randomImage = getRandomImage(imagesArray);
