6a723f1663222f0f9415ee5ecf1357d9073962d5
Brittany Hanlon <bmph1997@hotmail.com>
2023-11-20 21:40:37 -0330

    fixed console error message

--- mainScript.js
+++ mainScript.js
@@ -6,32 +6,32 @@ const resultMessage = document.getElementById("resultMessage");
 // const scoreElement = document.getElementById('currentScore');
 
 const keyValueMap = {
-  a: ["A","Images/alligator.svg", "10"],
-  b: ["B","Images/balloons.svg", "5"],
-  c: ["C","Images/cat.svg", "5"],
-  d: ["D","Images/dolphin.svg", "10"],
-  e: ["E","Images/elephant.svg", "5"],
-  f: ["F","Images/flower.svg", "5"],
-  g: ["G","Images/goat.svg", "10"],
-  h: ["H","Images/horse.svg", "5"],
-  i: ["I","Images/icecream.svg", "5"],
-  j: ["J","Images/jellyfish.svg", "10"],
-  k: ["K","Images/koala.svg", "10"],
-  l: ["L","Images/ladybug.svg", "5"],
-  m: ["M","Images/monkey.svg", "5"],
-  n: ["N","Images/numbers.svg", "5"],
-  o: ["O","Images/octopus.svg", "10"],
-  p: ["P","Images/penguin.svg", "10"],
-  q: ["Q","Images/queen.svg", "10"],
-  r: ["R","Images/raccoon.svg", "10"],
-  s: ["S","Images/snail.svg", "5"],
-  t: ["T","Images/turtle.svg", "5"],
-  u: ["U","Images/unicorn.svg", "10"],
-  v: ["V","Images/violin.svg", "10"],
-  w: ["W","Images/whale.svg", "10"],
-  x: ["X","Images/xylophone.svg", "10"],
-  y: ["Y","Images/yak.svg", "20"],
-  z: ["Z","Images/zebra.svg", "10"],
+  a: ["A", "Images/alligator.svg", "10"],
+  b: ["B", "Images/balloons.svg", "5"],
+  c: ["C", "Images/cat.svg", "5"],
+  d: ["D", "Images/dolphin.svg", "10"],
+  e: ["E", "Images/elephant.svg", "5"],
+  f: ["F", "Images/flower.svg", "5"],
+  g: ["G", "Images/goat.svg", "10"],
+  h: ["H", "Images/horse.svg", "5"],
+  i: ["I", "Images/icecream.svg", "5"],
+  j: ["J", "Images/jellyfish.svg", "10"],
+  k: ["K", "Images/koala.svg", "10"],
+  l: ["L", "Images/ladybug.svg", "5"],
+  m: ["M", "Images/monkey.svg", "5"],
+  n: ["N", "Images/numbers.svg", "5"],
+  o: ["O", "Images/octopus.svg", "10"],
+  p: ["P", "Images/penguin.svg", "10"],
+  q: ["Q", "Images/queen.svg", "10"],
+  r: ["R", "Images/raccoon.svg", "10"],
+  s: ["S", "Images/snail.svg", "5"],
+  t: ["T", "Images/turtle.svg", "5"],
+  u: ["U", "Images/unicorn.svg", "10"],
+  v: ["V", "Images/violin.svg", "10"],
+  w: ["W", "Images/whale.svg", "10"],
+  x: ["X", "Images/xylophone.svg", "10"],
+  y: ["Y", "Images/yak.svg", "20"],
+  z: ["Z", "Images/zebra.svg", "10"],
 };
 
 function buttonbox() {
@@ -54,15 +54,16 @@ function getRandomFirstValue(keyValueMap) {
     return values[1];
   } else {
     return null;
-  };
+  }
 }
+
 const firstRandomValue = getRandomFirstValue(keyValueMap);
 console.log(firstRandomValue);
 imageElement.src = firstRandomValue;
 
 function assignImage() {
   getRandomFirstValue(keyValueMap);
-  imageElement.setAttribute('src', firstRandomValue);
+  imageElement.setAttribute("src", firstRandomValue);
 
   if (time < 10) {
     greeting = "Good morning";
@@ -71,7 +72,7 @@ function assignImage() {
   } else {
     greeting = "Good evening";
   }
-};
+}
 
 // picks random letter in keyValueMap
 function getRandomSecondValue(keyValueMap) {
@@ -81,11 +82,11 @@ function getRandomSecondValue(keyValueMap) {
     return values[0];
   } else {
     return null;
-  };
   }
+}
 
-  const secondRandomValue = getRandomSecondValue(keyValueMap);
-  console.log(secondRandomValue);
+const secondRandomValue = getRandomSecondValue(keyValueMap);
+console.log(secondRandomValue);
 
 // picks score from keyValueMap
 function getRandomThirdValue(keyValueMap) {
@@ -95,7 +96,7 @@ function getRandomThirdValue(keyValueMap) {
     return values[2];
   } else {
     return null;
-  };
+  }
 }
 
 const thirdRandomValue = getRandomThirdValue(keyValueMap);
@@ -107,41 +108,19 @@ const showImageButton = document.getElementById("showImage");
 
 // Matches keyboard buttons to images
 function matchButtonIdToValue(buttonId) {
-  const buttonLetter = buttonId.toLowerCase(); 
+  const buttonLetter = buttonId.toLowerCase();
   const values = keyValueMap[buttonLetter];
   const matchedValue = values[0];
   if (matchedValue == secondRandomValue) {
-  // code that runs when correct answer is chosen
-   console.log("Good Job!")
-   alert("Good Job!")
+    // code that runs when correct answer is chosen
+    console.log("Good Job!");
+    alert("Good Job!");
   } else {
     // code that runs when incorrect answer is chosen
-    console.log("Keep Trying!")
-    alert("Keep Trying!")
+    console.log("Keep Trying!");
+    alert("Keep Trying!");
   }
 };
-
-const skipButton = document.getElementById("skip");
-
-skipButton.addEventListener("click", () => {
-  checkImage();
-})
-
-function checkImage () {
-  const matchedValue= getRandomSecondValue(keyValueMap);
-
-  if (matchedValue === secondRandomValue) {
-    assignImage(); //This function will assign a new random image
-  } else {
-    return null
-  }
-}
-
-function assignImage() {
-  const newRandomValue = getRandomFirstValue(keyValueMap);
-  imageElement.setAttribute('src', newRandomValue);
-  secondRandomValue = getRandomSecondValue(keyValueMap); // Update the secondRandomValue
-}
 
 // function for timer, when start clicked => restart
 var timer = document.getElementById("timer");
@@ -185,5 +164,15 @@ function openInfo() {
   welcomeMessage.style.display = "flex";
 }
 
+function buttonbox() {
+  document.getElementById("start");
+  document.getElementById("help");
+  document.getElementById("skip");
+}
 
+const skipButton = document.getElementById("skip");
+
+function nextImage() {
+
+}
 
